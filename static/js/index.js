7b2edc5f2b1b16b998c170dc30b3b18eadd9b676
Wil Bown <wilbown@gmail.com>
2018-05-09 20:23:20 -0600

    initial commit

---
+++ static/js/index.js
@@ -0,0 +1,29 @@
+var page = (function() {
+
+    var rdy = (function() {
+
+        // TODO Get data from Infura
+        
+        // console.log($('#modal_disclaimer').css('display'));
+
+        // $("#button_show_contract").click(function() {
+        //     $('#modal_contract').toggleClass('w3-show');
+        // });
+
+        // $("#button_risks_agree").click(function() {
+        //     $('#modal_risks').css('display','none');
+        // });
+        // $("#button_disclaimer_agree").click(function() {
+        //     $('#modal_disclaimer').css('display','none');
+        //     $('#modal_risks').css('display','block');
+        // });
+        // $('#modal_disclaimer').css('display','block');
+        
+    });
+
+    return {
+        rdy: rdy
+    };
+})();
+
+$(document).ready(page.rdy);
