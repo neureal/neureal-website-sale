f688e4db6aa5566c9a030f6699fff925cd1b576b
Wil Bown <wilbown@gmail.com>
2018-06-07 23:38:28 -0600

    successfully pulling blocknumber from Infura

--- static/js/index.js
+++ static/js/index.js
@@ -1,9 +1,67 @@
-var page = (function() {
 
+var eth_id = 1;
+
+function eth(method,params) {
+
+    var settings = {
+        'async': true,
+        'crossDomain': true,
+        'url': 'https://mainnet.infura.io',
+        'method': 'POST',
+        'headers': {},
+        "data": '{"jsonrpc":"2.0","id":'+(eth_id++)+',"method":"'+method+'","params":'+params+'}'
+      }
+        
+      $.ajax(settings).done(function(response) {
+        console.log(response);
+        $('#text_total_token').text(response.result);
+      });
+
+}
+
+var page = (function() {
     var rdy = (function() {
 
         // TODO Get data from Infura
+
+        eth('eth_blockNumber','[]');
+        var refreshId = setInterval(function() {
+            eth('eth_blockNumber','[]');
+        }, 10000);
+
+        // var settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://mainnet.infura.io",
+        //     "method": "POST",
+        //     "headers": {},
+        //     "data": "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_newFilter\",\"params\":[]}"
+        //     // "data": "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_blockNumber\",\"params\":[]}"
+        //   }
+          
+        //   $.ajax(settings).done(function(response) {
+        //     console.log(response);
+        //     // $('#text_total_token').text(response.result);
+        //   });
+
+
+        // var refreshId = setInterval(function() {
+        //     var r = (-0.5)+(Math.random()*(1000.99));
+        //     $('#img-container').load('images/gallery/best/random.php?'+r);
+        // }, 5000);
+
+        // $('#text_total_token').text('text_total_token Testing!!!');
+        // $('#text_total_eth').text('text_total_eth Testing!!!');
         
+
+        // $('#text_total_token').text((eth_id++));
+        // var refreshId = setInterval(function() {
+        //     $('#text_total_token').text((eth_id++));
+        // }, 10000);
+
+
+        // $("#text-status").load("/webhook_activate");
+
         // console.log($('#modal_disclaimer').css('display'));
 
         // $("#button_show_contract").click(function() {
@@ -20,10 +78,11 @@ var page = (function() {
         // $('#modal_disclaimer').css('display','block');
         
     });
-
-    return {
-        rdy: rdy
-    };
+    return { rdy: rdy };
 })();
-
 $(document).ready(page.rdy);
+
+// $(window).on('load', function() {
+//     console.log('OnLoad');
+//     // everything on page loaded
+// });
