412e7271473415d4b901bdfab19f2a3275b84c75
Aleksandr Sobolev <asobolev@zentist.io>
2018-06-21 19:21:28 +0600

    Initial logs feed API call

--- static/js/index.js
+++ static/js/index.js
@@ -1,88 +1,32 @@
+var ethId = 12;
 
-var eth_id = 1;
+const ws = new WebSocket("wss://mainnet.infura.io/ws")
 
-function eth(method,params) {
-
-    var settings = {
-        'async': true,
-        'crossDomain': true,
-        'url': 'https://mainnet.infura.io',
-        'method': 'POST',
-        'headers': {},
-        "data": '{"jsonrpc":"2.0","id":'+(eth_id++)+',"method":"'+method+'","params":'+params+'}'
-      }
-        
-      $.ajax(settings).done(function(response) {
-        console.log(response);
-        $('#text_total_token').text(response.result);
-      });
-
+ws.onopen = function() {
+  // sendEthGetLogs(1)
 }
 
-var page = (function() {
-    var rdy = (function() {
+ws.onmessage = function(message){
+  var data = JSON.parse(message.data);
+  displayTopicLogs(data.result)
+}
 
-        // TODO Get data from Infura
-
-        eth('eth_blockNumber','[]');
-        var refreshId = setInterval(function() {
-            eth('eth_blockNumber','[]');
-        }, 10000);
-
-        // var settings = {
-        //     "async": true,
-        //     "crossDomain": true,
-        //     "url": "https://mainnet.infura.io",
-        //     "method": "POST",
-        //     "headers": {},
-        //     "data": "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_newFilter\",\"params\":[]}"
-        //     // "data": "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_blockNumber\",\"params\":[]}"
-        //   }
-          
-        //   $.ajax(settings).done(function(response) {
-        //     console.log(response);
-        //     // $('#text_total_token').text(response.result);
-        //   });
+$(function(){
+  $('#displayTopic').click(function(){
+    const topicName = $('#id_topic').val();
+    getTopicLogs(ethId, topicName)
+  });
+})
 
 
-        // var refreshId = setInterval(function() {
-        //     var r = (-0.5)+(Math.random()*(1000.99));
-        //     $('#img-container').load('images/gallery/best/random.php?'+r);
-        // }, 5000);
-
-        // $('#text_total_token').text('text_total_token Testing!!!');
-        // $('#text_total_eth').text('text_total_eth Testing!!!');
-        
-
-        // $('#text_total_token').text((eth_id++));
-        // var refreshId = setInterval(function() {
-        //     $('#text_total_token').text((eth_id++));
-        // }, 10000);
+function getTopicLogs(ethId, topicName){
+  // const params = [{"address":["0x06012c8cf97BEaD5deAe237070F9587f8E7A266d"], "fromBlock": "0x6e616e6f706f6f6c2e6f7267", "toBlock": "0x65746865726d696e652d65753130"}]
+  const params = ['0x06012c8cf97BEaD5deAe237070F9587f8E7A266d', 'latest']
+  const message = JSON.stringify({jsonrpc: "2.0", id: ethId, method: 'eth_getBalance', params: params})
+  ws.send(message)
+}
 
 
-        // $("#text-status").load("/webhook_activate");
-
-        // console.log($('#modal_disclaimer').css('display'));
-
-        // $("#button_show_contract").click(function() {
-        //     $('#modal_contract').toggleClass('w3-show');
-        // });
-
-        // $("#button_risks_agree").click(function() {
-        //     $('#modal_risks').css('display','none');
-        // });
-        // $("#button_disclaimer_agree").click(function() {
-        //     $('#modal_disclaimer').css('display','none');
-        //     $('#modal_risks').css('display','block');
-        // });
-        // $('#modal_disclaimer').css('display','block');
-        
-    });
-    return { rdy: rdy };
-})();
-$(document).ready(page.rdy);
-
-// $(window).on('load', function() {
-//     console.log('OnLoad');
-//     // everything on page loaded
-// });
+function displayTopicLogs(logs){
+  console.log(logs)
+}
