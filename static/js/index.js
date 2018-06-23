f69c8a948b634f1a666cd64c11765fafe09eacc9
Aleksandr Sobolev <asobolev@zentist.io>
2018-06-23 16:50:34 +0600

    Cleanup PR regarding feedback

--- static/js/index.js
+++ static/js/index.js
@@ -1,57 +1,60 @@
-var ethId = 1;
-var dataFetcher;
+var $textTokenEl = '#text_token';
+var $textTokenTotalEth = '#text_total_eth';
 
-const infuraUrl = 'wss://ropsten.infura.io/ws/g97D5zS7v5gxGRgzQV60'
+let requestId = 1;
+let fromBlock = 'earliest';
+let toBlock = 'latest';
+let topics = ['0x19287f35bf9ce71d59481bf0e504fc7f02e898d429c85d11f5276bc24bd903c3'];
 
-
-const socket = new WebSocket(infuraUrl)
+const socket = new WebSocket('wss://ropsten.infura.io/ws/g97D5zS7v5gxGRgzQV60');
 
 socket.onopen = function() {
-  getTopicLogs()
-  dataFetcher = setInterval(function() {
-    getTopicLogs()
-  }, 10000);
+  socketSendMessage();
 }
 
 socket.onmessage = function(message){
   var data = JSON.parse(message.data);
-  displayTopicLogs(data.result)
+  var result = data.result;
+
+  if (!result.length)
+    return
+
+  var firstLog = result[0];
+  fromBlock = firstLog.blockNumber;
+  values = parseHex2Dig(firstLog.data);
+
+  $($textTokenEl).text(values[0]);
+  $($textTokenTotalEth).text(values[1]);
 }
+
+var dataFetcher;
 
 socket.onclose = function(){
-  clearInterval(dataFetcher)
+  clearInterval(dataFetcher);
 }
 
+dataFetcher = setInterval(function() {
+  socketSendMessage();
+}, 10000);
 
-const methodName = 'eth_getLogs'
-const topics = ['0x19287f35bf9ce71d59481bf0e504fc7f02e898d429c85d11f5276bc24bd903c3']
-let fromBlock = 'earliest'
-let toBlock = 'latest'
-
-function getTopicLogs(){
-  const message = JSON.stringify({
+function socketSendMessage(){
+  socket.send(JSON.stringify({
     "jsonrpc": "2.0",
-    "id": ethId,
-    "method": methodName,
+    "id": requestId,
+    "method": "eth_getLogs",
     "params": [{fromBlock, toBlock, topics}]
-  })
-
-  socket.send(message)
+  }));
+  requestId ++;
 }
 
+function parseHex2Dig(data) {
+  let values = data.replace('0x', '');
+  if (!values)
+    return [];
 
-function displayTopicLogs(logs){
-  let data = {}
-  if(logs.length > 0){
-    data = logs[0]
-    fromBlock = data.blockNumber
-    let values = data.data.replace('0x', '').match(/.{1,64}/g);
-    values.forEach(function(value, index) {
-      values[index] = parseInt(value, 16);
-    });
-    $('#text_token').text(values[1])
-    $('#text_total_eth').text(values[0])
-  }
+  values = values.match(/.{1,64}/g);
+  values.forEach((value, index) => {
+    values[index] = parseInt(value, 16);
+  });
+  return values;
 }
-
-
